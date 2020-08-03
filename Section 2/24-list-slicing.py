b4f1064a903b646c7d5411f349af7c111d41df06
Brandon Brown <hoobie4792@yahoo.com>
2020-08-03 17:58:37 -0400

    More learning

---
+++ Section 2/24-list-slicing.py
@@ -0,0 +1,16 @@
+friends = ["Rolf", "Charlie", "Anna", "Bob", "Jen"]
+
+# Output: ["Anna", "Bob"]
+print(friends[2:4])
+
+# Output: ['Charlie', 'Anna', 'Bob', 'Jen'] - From 1
+print(friends[1:])
+
+# Output: ['Rolf', 'Charlie', 'Anna', 'Bob'] - Up to 4
+print(friends[:4])
+
+# Output ["Anna", "Bob", "Jen"] - 3 from end to the end
+print(friends[-3:])
+
+# Output ["Rolf", "Charlie", "Anna"] - start to second to last element
+print(friends[-1:])
