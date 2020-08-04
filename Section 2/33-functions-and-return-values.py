6e80db3e5fc4e725b78a5d8e6f4d8a8407a32cbb
Brandon Brown <hoobie4792@yahoo.com>
2020-08-04 00:04:49 -0400

    More learning

--- Section 2/33-functions-and-return-values.py
+++ Section 2/33-functions-and-return-values.py
@@ -25,3 +25,13 @@ cars = [
 
 for car in cars:
   print(print_car_info(car))
+
+
+def divide(x, y):
+  if y == 0:
+    return "You tried to divide by zero!"
+  else:
+    return x / y
+
+print(divide(10, 2))
+print(divide(6, 0))
