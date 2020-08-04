8870cbb9853b0476c8763da8309b9cb6a1dff116
Brandon Brown <hoobie4792@yahoo.com>
2020-08-04 01:04:20 -0400

    More learning

---
+++ Section 4/37-intro-to-object-oriented-programming.py
@@ -0,0 +1,16 @@
+class Student:
+  def __init__(self, new_name, new_grade):
+    self.name = new_name
+    self.grades = new_grade
+
+  def average(self):
+    return sum(self.grades) / len(self.grades)
+
+
+student_one = Student('Rolf Smith', [70, 88, 90, 99])
+student_two = Student('Jose', [50, 60, 99, 100])
+
+print(student_one.name)
+print(student_two.name)
+print(student_one.average())
+print(student_two.average())
