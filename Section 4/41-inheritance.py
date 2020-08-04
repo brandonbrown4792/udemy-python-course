85fd40a6568e372b56a0305855371e9b04abb2ce
Brandon Brown <hoobie4792@yahoo.com>
2020-08-04 01:51:49 -0400

    More learning

---
+++ Section 4/41-inheritance.py
@@ -0,0 +1,31 @@
+class Student:
+  def __init__(self, name, school):
+    self.name = name
+    self.school = school
+    self.marks = []
+
+  def average(self):
+    return sum(self.marks) / len(self.marks)
+
+
+class WorkingStudent(Student):
+  def __init__(self, name, school, salary):
+    super().__init__(name, school)
+    self.salary = salary
+
+  def weekly_salary(self):
+    return self.salary * 37.5
+
+
+rolf = WorkingStudent('Rolf', 'MIT', 15.50)
+print(rolf.name)
+print(rolf.school)
+print(rolf.salary)
+rolf.marks.append(57)
+rolf.marks.append(99)
+print(rolf.average())
+print(rolf.weekly_salary())
+
+# Will not work because weekly_salary is not defined for Student class
+# anna = Student('Anna', 'Oxford')
+# print(anna.weekly_salary())
