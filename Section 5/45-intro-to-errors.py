9ec2cfbd0e4f61e72127819396656fec237048d6
Brandon Brown <hoobie4792@yahoo.com>
2020-08-04 10:47:02 -0400

    More learning

---
+++ Section 5/45-intro-to-errors.py
@@ -0,0 +1,29 @@
+# NameError
+# print(my_variable)
+
+
+
+
+
+# TypeError
+# def add_movie():
+#   name = input('Enter the movie name: ')
+#   director = input ('Enter the movie director: ')
+#   year = input('Enter the movie year: ')
+
+#   movies.append({
+#     'name': name
+#     'director': director
+#     'year': year
+#   })
+
+
+# def show_movies(movies_list):
+#   for movie in movies_list:
+#     show_movie_details(movie)
+
+
+# def show_movie_details(movie):
+#   print(f"Name: {movies['name']}") # <------ movies should be movie
+#   print(f"Name: {movie['director']}")
+#   print(f"Name: {movie['year']}")
