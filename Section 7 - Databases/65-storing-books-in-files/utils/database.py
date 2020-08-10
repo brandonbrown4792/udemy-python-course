3ce34b9d9ebffe8c45a0440f15d8284dc2aa9812
Brandon Brown <hoobie4792@yahoo.com>
2020-08-10 17:38:39 -0400

    More learning

--- Section 7 - Databases/65-storing-books-in-files/utils/database.py
+++ Section 7 - Databases/65-storing-books-in-files/utils/database.py
@@ -2,15 +2,16 @@
 Concerned with storing and retrieving books from a csv file.
 Format of the csv file:
 
-name,author,read\n
+name, author, read
 """
+
 
 books_file = 'books.txt'
 
 
 def create_book_table():
     with open(books_file, 'w'):
-        pass  # Just to make sure file is present on initial reads
+        pass
 
 
 def add_book(name, author):
@@ -22,18 +23,18 @@ def get_all_books():
     with open(books_file, 'r') as file:
         lines = [line.strip().split(',') for line in file.readlines()]
 
-    books = [
+    return [
         {'name': line[0], 'author': line[1], 'read': line[2]}
         for line in lines
     ]
-    return books
 
 
 def mark_book_as_read(name):
     books = get_all_books()
     for book in books:
         if book['name'] == name:
-            book['read'] = '1'
+            book['read'] = 1
+
     _save_all_books(books)
 
 
