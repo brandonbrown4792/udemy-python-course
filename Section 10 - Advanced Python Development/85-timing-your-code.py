573315e40bf4a72ac95d32511026ebed59f76ca0
Brandon Brown <hoobie4792@yahoo.com>
2020-08-22 00:20:15 -0400

    More learning

---
+++ Section 10 - Advanced Python Development/85-timing-your-code.py
@@ -0,0 +1,23 @@
+import timeit
+import time
+
+
+def measure_runtime(func):
+    start = time.time()
+    func()
+    end = time.time()
+    print(end - start)
+
+
+def powers(limit):
+    return [x**2 for x in range(limit)]
+
+
+measure_runtime(lambda: powers(5000000))
+
+
+# Timeit accepts a python statement as a string
+# It makes many iterations on the statement
+print(timeit.timeit("[x**2 for x in range(10)]"))
+print(timeit.timeit("list(map(lambda x: x**2, range(10)))"))
+# This exercise shows that list comprehension is faster
