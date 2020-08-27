67d7403c6e42981a24a3dfa8f9a50e6a810a274a
Brandon Brown <hoobie4792@yahoo.com>
2020-08-27 17:36:28 -0400

    More learning

--- Section 11 - Webscraping/95-scraping-books/menu.py
+++ Section 11 - Webscraping/95-scraping-books/menu.py
@@ -1,4 +1,8 @@
+import logging
+
 from app import books
+
+logger = logging.getLogger('scraping.menu')
 
 
 USER_CHOICE = '''Enter one of the following
@@ -12,18 +16,21 @@ Enter your choice: '''
 
 
 def print_best_books():
+    logger.info('Finding best books by rating...')
     best_books = sorted(books, key=lambda x: (x.rating * -1, x.price))[:10]  # Sorted ascending by default. Sort by rating, then by price
     for book in best_books:
         print(book)
 
 
 def print_worst_books():
+    logger.info('Finding worst books by rating...')
     best_books = sorted(books, key=lambda x: x.rating)[:10]
     for book in best_books:
         print(book)
 
 
 def print_cheapest_books():
+    logger.info('Finding cheapest books by price...')
     cheapest_books = sorted(books, key=lambda x: x.price)[:10]
     for book in cheapest_books:
         print(book)
@@ -33,6 +40,7 @@ books_generator = (x for x in books)
 
 
 def print_next_book():
+    logger.info('Getting next book...')
     print(next(books_generator))
 
 
@@ -52,6 +60,7 @@ def menu():
             print('Please enter a valid command')
 
         user_input = input(USER_CHOICE)
+    logger.debug('Terminating program...')
 
 
 menu()
