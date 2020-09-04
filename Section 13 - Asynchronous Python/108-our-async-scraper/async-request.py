d74534046b3c7fea146d169e3d2f067df9191f9d
Brandon Brown <hoobie4792@yahoo.com>
2020-09-04 14:27:53 -0400

    More learning

--- Section 13 - Asynchronous Python/108-our-async-scraper/async-request.py
+++ Section 13 - Asynchronous Python/108-our-async-scraper/async-request.py
@@ -3,18 +3,25 @@ import asyncio
 import time
 
 
-async def fetch_page(url):
-    # Client sessions are a connection pool (allows us to use multiple connections at the same time)
-    async with aiohttp.ClientSession() as session:
-        page_start = time.time()
-        async with session.get(url) as response:
-            print(f'Page took {time.time() - page_start}')
-            return response.status
+async def fetch_page(session, url):
+    page_start = time.time()
+    async with session.get(url) as response:
+        print(f'Page took {time.time() - page_start}')
+        return response.status
+
+
+async def get_multiple_ages(loop, *urls):
+    tasks = []
+    async with aiohttp.ClientSession(loop=loop) as session:
+        for url in urls:
+            tasks.append(fetch_page(session, url))
+        grouped_tasks = asyncio.gather(*tasks)
+        return await grouped_tasks
 
 
 loop = asyncio.get_event_loop()
 
-tasks = [fetch_page('http://google.com') for i in range(50)]
+urls = ['http://google.com' for i in range(50)]
 start = time.time()
-loop.run_until_complete(asyncio.gather(*tasks))
+loop.run_until_complete(get_multiple_ages(loop, *urls))
 print(f'All took {time.time() - start}')
