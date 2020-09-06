792fae170c468e3642bd3e61d0d7801be42bdfb0
brandonbrown4792 <brandonbrown4792@gmail.com>
2020-09-06 01:14:45 -0400

    More learning

---
+++ Section 16 - Interacting with APIs/117-caching-funcitons-with-functools/app.py
@@ -0,0 +1,27 @@
+import time
+from openexchange import OpenExchangeClient
+
+APP_ID = '8e3721a8892a499389a3578a053a6e7a'
+
+client = OpenExchangeClient(APP_ID)
+
+usd_amount = 1000
+start = time.time()
+gbp_amount = OpenExchangeClient.convert(client, usd_amount, 'USD', 'GBP')
+end = time.time()
+
+print(end - start)
+
+start = time.time()
+gbp_amount = OpenExchangeClient.convert(client, usd_amount, 'USD', 'GBP')
+end = time.time()
+
+print(end - start)
+
+start = time.time()
+gbp_amount = OpenExchangeClient.convert(client, usd_amount, 'USD', 'GBP')
+end = time.time()
+
+print(end - start)
+
+print(f'USD: {usd_amount}, GBP: {gbp_amount:.2f}')
---
+++ Section 16 - Interacting with APIs/117-caching-funcitons-with-functools/openexchange.py
@@ -0,0 +1,25 @@
+import functools
+import requests
+
+
+class OpenExchangeClient:
+    BASE_URL = 'https://openexchangerates.org/api'
+
+    def __init__(self, app_id):
+        self.app_id = app_id
+
+    @property
+    @functools.lru_cache(maxsize=2)
+    # As long as the arguments don't change, functools will use the cached value
+    def latest(self):
+        return requests.get(f'{self.BASE_URL}/latest.json?app_id={self.app_id}').json()
+
+    def convert(self, from_amount, from_currency, to_currency):
+        rates = self.latest['rates']
+        to_rate = rates[to_currency]
+
+        if from_currency == 'USD':
+            return from_amount * to_rate
+        else:
+            from_in_usd = from_amount / rates[from_currency]
+            return from_in_usd * to_rate
