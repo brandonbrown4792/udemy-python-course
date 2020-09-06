da7589429dd06956e31ccaa59bfcb96e7c020d37
brandonbrown4792 <brandonbrown4792@gmail.com>
2020-09-05 23:43:25 -0400

    More learning

--- Section 15 - Web Development with Flask/my-first-flask-app/app.py
+++ Section 15 - Web Development with Flask/my-first-flask-app/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
 posts = {
     0: {
+        'id': 0,
         'title': 'Hello, world',
         'content': 'This is my first blog post!'
     }
@@ -26,8 +27,18 @@ def post(post_id):
         return render_template('404.html', message=f'A post with post id {post_id} was not found.')
 
 
+@app.route('/posts/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        post_id = len(posts)
+        posts[post_id] = {'id': post_id, 'title': title, 'content': content}
+
+        return redirect(url_for('post', post_id=post_id))
+
+    return render_template('create.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# Comment
