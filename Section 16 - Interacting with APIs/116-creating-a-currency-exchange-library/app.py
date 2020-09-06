05d1ce34042c4eec37f579c387f014a0dd06c8c6
brandonbrown4792 <brandonbrown4792@gmail.com>
2020-09-06 01:04:28 -0400

    More learning

---
+++ Section 16 - Interacting with APIs/116-creating-a-currency-exchange-library/app.py
@@ -0,0 +1,10 @@
+from openexchange import OpenExchangeClient
+
+APP_ID = '8e3721a8892a499389a3578a053a6e7a'
+
+client = OpenExchangeClient(APP_ID)
+
+usd_amount = 1000
+gbp_amount = OpenExchangeClient.convert(client, usd_amount, 'USD', 'GBP')
+
+print(f'USD: {usd_amount}, GBP: {gbp_amount}')
