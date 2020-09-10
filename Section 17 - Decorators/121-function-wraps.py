3167d3bf662b59ebb66cf8898d2257945ca8cc74
Brandon Brown <hoobie4792@yahoo.com>
2020-09-10 11:49:51 -0400

    More learning

---
+++ Section 17 - Decorators/121-function-wraps.py
@@ -0,0 +1,26 @@
+import functools
+
+user = {'username': 'jose123', 'access_level': 'admin'}
+
+def user_has_permission(func):
+    @functools.wraps(func)
+    def secure_func():
+        if user.get('access_level') == 'admin':
+            return func()
+    return secure_func
+
+@user_has_permission
+def my_function():
+    """
+    Allows us to retrieve the password fr the admin panel.
+    """
+    return 'Password for admin panel is 1234.'
+
+@user_has_permission
+def another():
+    pass
+
+
+print(my_function())
+print(my_function.__name__)  # Returns secure_func
+print(another.__name__)  # Returns secure_func
