c457fe1fc1b945ec8a94c1b900a8e675bb1208aa
Brandon Brown <hoobie4792@yahoo.com>
2020-09-22 23:53:21 -0400

    More learning

--- Section 19 - GUI Development with Tkinker/135-text-editor-project/app.py
+++ Section 19 - GUI Development with Tkinker/135-text-editor-project/app.py
@@ -1,5 +1,7 @@
+import os
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog
 
 
 def create_file():
@@ -7,6 +9,24 @@ def create_file():
     text_area.pack(fill='both', expand=True)
     notebook.add(text_area, text='Untitled')
     notebook.select(text_area)
+
+
+def save_file():
+    file_path = filedialog.asksaveasfilename()
+
+    try:
+        filename = os.path.basename(file_path)
+        text_widget = root.nametowidget(notebook.select())
+        content = text_widget.get('1.0', 'end-1c')
+
+        with open(file_path, 'w') as file:
+            file.write(content)
+
+    except(AttributeError, FileNotFoundError):
+        print('Save operation cancelled')
+        return
+
+    notebook.tab('current', text=filename)
 
 
 root = tk.Tk()
@@ -23,6 +43,7 @@ file_menu = tk.Menu(menubar)
 menubar.add_cascade(menu=file_menu, label='File')
 
 file_menu.add_command(label='New', command=create_file)
+file_menu.add_command(label='Save', command=save_file)
 
 notebook = ttk.Notebook(main)
 notebook.pack(fill='both', expand=True)
